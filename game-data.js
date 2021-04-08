fb122e29cd248ed8e9f82c2211c66afdfd330631
cicerakes <45453664+cicerakes@users.noreply.github.com>
2021-04-08 19:31:40 +1000

    Add THE iDOLM@STER MILLION LIVE! THEATER DAYS (JP) server
    Closes #42

--- game-data.js
+++ game-data.js
@@ -410,6 +410,12 @@ const gameData = [
 		dailyReset: "05:00"
 	},
 	{
+		game: "THE iDOLM@STER MILLION LIVE! THEATER DAYS",
+		server: "JP",
+		timezone: "Asia/Tokyo",
+		dailyReset: "00:00"
+	},
+	{
 		game: "The Seven Deadly Sins: Grand Cross",
 		server: "Global",
 		timezone: "Etc/GMT+7",
