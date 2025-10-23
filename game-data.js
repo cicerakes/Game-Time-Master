0901e7ed2cb4af9bcb9cf055ba86e0ae5ab55337
cicerakes <45453664+cicerakes@users.noreply.github.com>
2025-10-23 22:15:01 +1100

    Add Mecharashi (Global)
    Closes #716

--- game-data.js
+++ game-data.js
@@ -1339,6 +1339,13 @@ var gameData = [
 		icon: "maplestory-m"
 	},
 	{
+		game: "Mecharashi",
+		server: "Global",
+		timezone: "Etc/GMT+7",
+		dailyReset: "00:00",
+		icon: "mecharashi"
+	},
+	{
 		game: "MementoMori",
 		server: "EU/Global",
 		timezone: "Etc/GMT-1",
